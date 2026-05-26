a131bc94251010515bbded10be46c840cee58c9b
Sam7895 <sameerpatil2502@gmail.com>
2026-05-26 16:58:03 +0530

    D: refactor

--- main.go
+++ main.go
@@ -1,29 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Enter a command: ")
-		scanner.Scan()
-		text := scanner.Text()
-		cleaned := cleanInput(text)
-		output := cleaned[0]
-		if output == "" {
-			continue
-		}
-		fmt.Println("Your command was:", output)
-		if output == "exit" {
-			fmt.Println("Exiting...")
-			os.Exit(0)
-		}
-		switch output {
-		case "":
-		}
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,9 +1,36 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Enter a command: ")
+		scanner.Scan()
+		text := scanner.Text()
+		cleaned := cleanInput(text)
+		output := cleaned[0]
+		if output == "" {
+			continue
+		}
+		fmt.Println("Your command was:", output)
+		if output == "exit" {
+			fmt.Println("Exiting...")
+			os.Exit(0)
+		}
+		switch output {
+		case "":
+		}
+	}
 }
